c982854becc6c3451f00afb29b819f59c871c46b
Makaite <865600317@qq.com>
2024-04-16 22:25:30 +0800

    feat:增加双端队列

--- queue/02_doubleQueue.js
+++ queue/02_doubleQueue.js
@@ -12,13 +12,21 @@ class doubleQueue {
   }
 
   //从队前进队
-  enFontQueue(value) {
+  enFrontQueue(value) {
     //空队列时
     if(this.isEmpty()) {
       this.enBackQueue(value)
+    } else if(this.lowestCount > 0) {
+      this.lowestCount--
+      this.queue[this.lowestCount] = value
+    } else {
+      for(let i = this.count; i > 0; i--) {
+        this.queue[i] = this.queue[i - 1]
+      }
+      this.count++
+      this.lowestCount = 0
+      this.queue[0] = value
     }
-    this.queue[this.lowestCount] = value
-    this.lowestCount--
   }
 
   //从队后进队
@@ -27,9 +35,76 @@ class doubleQueue {
     this.count++
   }
 
+  //从队前离队
+  removeFront() {
+    if(this.isEmpty()) {
+      return ''
+    }
+    const result = this.queue[this.lowestCount]
+    delete this.queue[this.lowestCount]
+    this.lowestCount++
+    return result
+  }
+
+  //从队后离队
+  removeBack() {
+    if(this.isEmpty()) {
+      return ''
+    }
+    --this.count
+    const result = this.queue[this.count]
+    delete this.queue[this.count]
+    return result
+  }
+
   //是否为空队列
   isEmpty() {
     return this.count - this.lowestCount === 0
   }
 
+  //队列长度
+  size() {
+    return this.count - this.lowestCount
+  }
+
+  //队头
+  start() {
+    if(this.isEmpty()) {
+      return ''
+    }
+    return this.queue[this.lowestCount]
+  }
+
+  //toString方法
+  toString() {
+    if(this.isEmpty()) {
+      return ''
+    }
+    let str = ''
+    for(let key in this.queue) {
+      str += this.queue[key]
+    }
+    return str
+  }
 }
+
+const queue = new doubleQueue()
+queue.enFrontQueue(9)
+queue.enFrontQueue(8)
+queue.enBackQueue(7)
+console.log(queue.toString());
+console.log(queue.size());
+
+queue.removeFront()
+console.log(queue.toString());
+console.log(queue.size());
+
+queue.removeBack()
+console.log(queue.toString());
+console.log(queue.size());
+
+queue.enFrontQueue(6)
+queue.enFrontQueue(5)
+console.log(queue.toString());
+console.log(queue.size());
+
