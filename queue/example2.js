830bbd7577580065d135c8f3fb5f677e4f7ad668
Makaite <865600317@qq.com>
2024-04-18 22:01:18 +0800

    feat:增加队列应用-回文检查

--- queue/example2.js
+++ queue/example2.js
@@ -4,21 +4,39 @@
  * 
  =======================================*/
 
- class singleQueue {
+ class doubleQueue {
   constructor() {
     this.count = 0
     this.lowestCount = 0
     this.queue = {}
   }
 
-  //进队
-  enqueue(value) {
+  //从队前进队
+  enFrontQueue(value) {
+    //空队列时
+    if(this.isEmpty()) {
+      this.enBackQueue(value)
+    } else if(this.lowestCount > 0) {
+      this.lowestCount--
+      this.queue[this.lowestCount] = value
+    } else {
+      for(let i = this.count; i > 0; i--) {
+        this.queue[i] = this.queue[i - 1]
+      }
+      this.count++
+      this.lowestCount = 0
+      this.queue[0] = value
+    }
+  }
+
+  //从队后进队
+  enBackQueue(value) {
     this.queue[this.count] = value
     this.count++
   }
 
-  //离队
-  dequeue() {
+  //从队前离队
+  removeFront() {
     if(this.isEmpty()) {
       return ''
     }
@@ -28,12 +46,15 @@
     return result
   }
 
-  //队头
-  start() {
+  //从队后离队
+  removeBack() {
     if(this.isEmpty()) {
       return ''
     }
-    return this.queue[this.lowestCount]
+    --this.count
+    const result = this.queue[this.count]
+    delete this.queue[this.count]
+    return result
   }
 
   //是否为空队列
@@ -46,14 +67,15 @@
     return this.count - this.lowestCount
   }
 
-  //清空队列
-  clear() {
-    this.queue = {}
-    this.count = 0
-    this.lowestCount = 0
+  //队头
+  start() {
+    if(this.isEmpty()) {
+      return ''
+    }
+    return this.queue[this.lowestCount]
   }
 
-  //toString
+  //toString方法
   toString() {
     if(this.isEmpty()) {
       return ''
@@ -67,3 +89,27 @@
 }
 
 //回文检查处理函数
+function palindromeChecker(str) {
+  if(str === undefined || str === null || (str !== null && str.length === 0)) {
+    return false
+  }
+
+  const queue = new doubleQueue()
+
+  const targetStr = str.toLocaleLowerCase().split(' ').join('')
+  let isEqual = true
+  let firstCharm, lastChar
+  for(let i = 0; i < targetStr.length; i++) {
+    queue.enBackQueue(targetStr.charAt(i))
+  }
+  while(queue.size() > 1 && isEqual) {
+    firstCharm = queue.removeFront()
+    lastChar = queue.removeBack()
+    if(firstCharm !== lastChar) {
+      isEqual = false
+    }
+  }
+  return isEqual
+}
+
+console.log(palindromeChecker('abcba'));
